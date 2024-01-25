35129015059caf7151398c46d2e1aefd4558abc8
kabir shuvo <kabirshuvo19@gmail.com>
2024-01-25 17:39:55 +0600

    Logo added and about me uotline created

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,8 +10,30 @@ const AboutPage = () => {
       <Navbar />
       <SectionLayout>
        <div>
-        <h3>About Page</h3>
+        <h3>
+          Kamrul Hasan <br />
+          Print on demand Specialist & <br />
+          Graphic Designer.
+        </h3>
        </div>
+      </SectionLayout>
+      <SectionLayout>
+        <div>
+          <div>left div</div>
+          <div>Image div</div>
+          <div>Rirgt div</div>
+        </div>
+      </SectionLayout>
+      <SectionLayout>
+        <div>
+          <div>image 1</div>
+          <div>image 2</div>
+          <div>image 3</div>
+          <div>image 4</div>
+          <div>image 5</div>
+          <div>image 6</div>
+          <div>image 7</div>
+        </div>
       </SectionLayout>
       <Footer />
     </>
