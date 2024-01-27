42535a32aa6786f63d2bc6d4d8759ff40e6ca9cf
kabir shuvo <kabirshuvo19@gmail.com>
2024-01-27 10:35:57 +0600

    in Styles folder containe component added

---
+++ app/Components/styles/Container.styled.js
@@ -0,0 +1,11 @@
+"use client"
+
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 1240px;
+    max-width: 100%;
+    padding: 0 20px;
+    margin: 0 auto;
+`
+
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import Navbar from "../Components/Header/NavBar";
 import Footer from "../Components/Footer";
 import SectionLayout from "../Components/SectionLayout";
+import {Container} from "../Components/styles/Container.styled"
+
 
 const ServicesPage = () => {
   return (
     <div>
       <Navbar />
-      <SectionLayout>
+      <Container>
         <div>
           <h3>Services Page</h3>
         </div>
-      </SectionLayout>
+      </Container>
       <Footer />
     </div>
   );
