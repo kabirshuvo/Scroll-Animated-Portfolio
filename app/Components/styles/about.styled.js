53775be7182b79a85bb69cc7f9cf548f852830f3
kabir shuvo <kabirshuvo19@gmail.com>
2024-01-27 10:44:57 +0600

    About Page in now server component

--- app/Components/styles/about.styled.js
+++ app/Components/styles/about.styled.js
@@ -1,4 +1,61 @@
-export const AboutLaout = () => `
+"use client"
 
-width: 100%
-`
+import styled from "styled-components"
+import { motion, useScroll, useTransform } from "framer-motion";
+
+
+export const CenteredText = styled.h3`
+  text-align: center;
+  font-size: 80px;
+  background: linear-gradient(45deg, #ff8a00, #e52e71, #3498db);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const LeftDiv = styled.div`
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ImageDiv = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+
+  img {
+    width: 30rem;
+    height: 46rem;
+    border-radius: 5% 1% 40% 40%; /* Example: Add border-radius for a rounded image */
+    /* Add other styles for the image here */
+  }
+`;
+
+export const RightDiv = styled.div`
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+
+`;
+
+export const ScrollingImages = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  overflow-x: auto;
+  
+`;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import Navbar from "../Components/Header/NavBar";
 import Footer from "../Components/Footer";
 import SectionLayout from "../Components/SectionLayout";
+import {CenteredText, FlexContainer, LeftDiv, ImageDiv, RightDiv, ScrollingImages} from "../Components/styles/about.styled"
 import { motion, useScroll, useTransform } from "framer-motion";
-import styled from "styled-components";
 
 const AboutPage = () => {
   return (
@@ -46,58 +46,4 @@ const AboutPage = () => {
 
 export default AboutPage;
 
-const CenteredText = styled.h3`
-  text-align: center;
-  font-size: 80px;
-  background: linear-gradient(45deg, #ff8a00, #e52e71, #3498db);
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-`;
 
-const FlexContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const LeftDiv = styled.div`
-  width: 25%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ImageDiv = styled.div`
-  width: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0 auto;
-
-  img {
-    width: 30rem;
-    height: 46rem;
-    border-radius: 5% 1% 40% 40%; /* Example: Add border-radius for a rounded image */
-    /* Add other styles for the image here */
-  }
-`;
-
-const RightDiv = styled.div`
-  width: 25%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-
-`;
-
-const ScrollingImages = styled(motion.div)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-  overflow-x: auto;
-  
-`;
